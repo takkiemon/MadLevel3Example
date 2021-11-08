12bb53e18d24e4608af40ec204dfd39b9fc49e65
TakTufLaptop <takkie91@hotmail.com>
2021-11-08 13:08:58 +0100

    remove ? return operator

--- app/src/main/java/com/example/madlevel3example/RemindersFragment.kt
+++ app/src/main/java/com/example/madlevel3example/RemindersFragment.kt
@@ -22,7 +22,7 @@ class RemindersFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentRemindersBinding.inflate(inflater, container, false)
         return binding.root
