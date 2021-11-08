30beb4381740874fabfac19e7cd0775f012a36f2
TakTufLaptop <takkie91@hotmail.com>
2021-11-08 14:47:00 +0100

    implement addReminder logic.

--- app/src/main/java/com/example/madlevel3example/AddReminderFragment.kt
+++ app/src/main/java/com/example/madlevel3example/AddReminderFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
 import com.example.madlevel3example.databinding.FragmentAddReminderBinding
+
+const val REQ_REMINDER_KEY = "req_reminder"
+const val BUNDLE_REMINDER_KEY = "bundle_reminder"
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -22,19 +28,40 @@ class AddReminderFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentAddReminderBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAddReminder.setOnClickListener {
+            onAddReminder()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun onAddReminder() {
+        val reminderText = binding.etReminderName.text.toString()
+
+        if (reminderText.isNotBlank()) {
+            //set the data as fragmentResult, we are listening for REQ_REMINDER_KEY in RemindersFragment!
+            setFragmentResult(REQ_REMINDER_KEY, bundleOf(Pair(BUNDLE_REMINDER_KEY, reminderText)))
+
+            //"pop" the backstack, this means we destroy
+            //this fragment and go back to the RemindersFragment
+            findNavController().popBackStack()
+
+        } else {
+            Toast.makeText(
+                activity,
+                R.string.not_valid_reminder, Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
