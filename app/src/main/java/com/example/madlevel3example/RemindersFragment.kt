2af95ff3277313a9cf1ccfd6212a97bdd94c4a72
TakTufLaptop <takkie91@hotmail.com>
2021-11-08 13:40:04 +0100

    setup OnCreateView() in RemindersFragment.kt. clean up any references to the binding class instance of the fragment in OnDestroyView(). add list and adapter of Reminders. add InitViews().

--- app/src/main/java/com/example/madlevel3example/RemindersFragment.kt
+++ app/src/main/java/com/example/madlevel3example/RemindersFragment.kt
@@ -6,13 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel3example.databinding.FragmentRemindersBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class RemindersFragment : Fragment() {
-
+    private val reminders = arrayListOf<Reminder>()
+    private val reminderAdapter = ReminderAdapter(reminders)
     private var _binding: FragmentRemindersBinding? = null
 
     // This property is only valid between onCreateView and
@@ -31,10 +34,19 @@ class RemindersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViews()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initViews() {
+        // Initialize the recycler view with a linear layout manager, adapter
+        binding.rvReminders.layoutManager =
+            LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        binding.rvReminders.adapter = reminderAdapter
     }
 }
